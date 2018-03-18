41171ebd2b41216b19cbe6a758bc2b89cd7790c4
Saulo Santiago <saulodasilvasantiago@gmail.com>
2018-03-17 22:56:56 -0300

    add dinamic routes to check if user is logged on init app

--- src/index.js
+++ src/index.js
@@ -4,14 +4,34 @@
  * @flow
  */
 
-import React from 'react';
+import React, { Component } from 'react';
+import { AsyncStorage } from 'react-native';
 
 import 'config/ReactotronConfig';
 import 'config/DevToolsConfig';
-import Routes from 'routes';
+import createNavigator from './routes';
 
-const App = () => (
-  <Routes />
-);
+export default class App extends Component {
+  state = {
+    userChecked: false,
+    userLogged: false,
+  }
 
-export default App;
+  async componentDidMount() {
+    const username = await AsyncStorage.getItem('@Githuber:username');
+    this.appLoaded(username);
+  }
+
+  appLoaded = (username) => {
+    this.setState({
+      userChecked: true,
+      userLogged: !!username,
+    });
+  }
+
+  render() {
+    if (!this.state.userChecked) return null;
+    const Routes = createNavigator(this.state.userLogged);
+    return <Routes />;
+  }
+}
