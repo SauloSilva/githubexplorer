536e6cd96df85b4f2c494f07ecaeaeb9fb2c1ac7
Saulo Santiago <saulodasilvasantiago@gmail.com>
2018-03-18 03:55:35 -0300

    add logic to organization screen

--- src/pages/organizations/index.js
+++ src/pages/organizations/index.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-import { View } from 'react-native';
+import { View, Text, AsyncStorage, FlatList, ActivityIndicator } from 'react-native';
+import api from 'services/api';
+import styles from './styles';
 
 export default class Organizations extends Component {
   static navigationOptions = {
@@ -10,9 +12,43 @@ export default class Organizations extends Component {
     tabBarIcon: ({ tintColor }) => <Icon name="building" size={20} color={tintColor} />,
   };
 
+  state = {
+    data: [],
+    loading: true,
+  }
+
+  componentDidMount() {
+    this.loadOrganizations();
+  }
+
+  loadOrganizations = async () => {
+    const username = await AsyncStorage.getItem('@Githuber:username');
+    const response = await api.get(`/users/${username}/orgs`);
+
+    this.setState({ data: response.data, loading: false });
+  }
+
+  renderListItem = ({ item }) => (
+    <Text>{item.login}</Text>
+  );
+
+  renderList = () => (
+    <FlatList
+      data={this.state.data}
+      keyExtractor={item => String(item.id)}
+      renderItem={this.renderListItem}
+    />
+  );
+
   render() {
     return (
-      <View />
+      <View style={styles.container}>
+        {
+          this.state.loading
+            ? <ActivityIndicator style={styles.loading} />
+            : this.renderList()
+        }
+      </View>
     );
   }
 }
